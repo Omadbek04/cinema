f2a6066a58e0bd7df447b6035047ffd387126975
omadbek <abdullayevomadbek709@gmail.com>
2024-07-21 23:54:40 +0500

    header bg image qoyildi va bg gradientham qoshildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import heroBg from "../assets/heroBg.png";
+import heroBg from "../assets/hero.jpg";
 import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <header style={{ backgroundImage: `url(${heroBg})` }} className="h-screen">
-      <nav className="bg-transparent">
+      <nav className="bg-transparent relative z-10">
         <div className="container  mx-auto pr-4 ">
           <div className=" flex flex-wrap items-center justify-between">
             <Link to="/" className=" ">
@@ -32,12 +32,12 @@ const Navbar = () => {
 
               <div className="relative block">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg className="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                   </svg>
                   <span className="sr-only">Search icon</span>
                 </div>
-                <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm bg-transparent focus:outline-none outline-none text-gray-300 border border-gray-300 rounded-lg bg-gray-50" placeholder="Search..." />
+                <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm bg-transparent focus:outline-none outline-none text-white border border-white rounded-lg bg-white" placeholder="Search..." />
               </div>
             </div>
           </div>
@@ -48,6 +48,13 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
+      <div 
+      className="default-ltr-cache-1y1tnl9 egicmjq0" 
+      style={{
+        backgroundImage: "linear-gradient(to top, rgba(0, 0, 0 ,0.8) 0%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0,0.2) 100%)"
+      }} 
+      data-uia="nmhp-card-hero+gradient">
+    </div>
     </header>
   );
 };
